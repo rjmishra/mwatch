2b4f94a51f30adcbd699cbbc32cd67e026be24bb
Ranjan Mishra <rjmishra111@gmail.com>
2023-10-18 19:59:43 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import requests
 import pandas as pd
 import streamlit as st
-import os.path
+#import os.path
+import time
 from datetime import datetime
 
 sec_mappings = {"NIFTY 50" : "NIFTY", 
@@ -18,56 +19,61 @@ ncols = [x.upper() for x in cols]
 
 df_dict = {}
 headers={"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36"}
-res=requests.get(r"https://www.nseindia.com/api/live-analysis-variations?index=gainers",headers=headers)
-gainer_dict = {}
-for key in res.json():
-    if key != 'legends':
-        df = pd.DataFrame(res.json()[key]['data'])
+st_code = 401
+while st_code != 200:
+    time.sleep(2)
+    res=requests.get(r"https://www.nseindia.com/api/live-analysis-variations?index=gainers",headers=headers)
+    st_code = res.status_code
+if res.status_code == 200:
+    gainer_dict = {}
+    for val in sec_mappings.values():
+        df = pd.DataFrame(res.json()[val]['data'])
         if df.shape[0] > 0:
             # df.columns
             dff = df[cols]
             dff.columns = ncols
             dff['STATUS'] = 'Gainer'
-            gainer_dict[key] = dff
+            gainer_dict[val] = dff
         else:
-            gainer_dict[key] = df
+            gainer_dict[val] = df
+    df_dict['Gainers'] = gainer_dict
 looser_dict = {}
-res=requests.get(r"https://www.nseindia.com/api/live-analysis-variations?index=loosers",headers=headers)
-for key in res.json():
-    if key != 'legends':
-        df = pd.DataFrame(res.json()[key]['data'])
+st_code = 401
+while st_code != 200:
+    time.sleep(2)
+    res=requests.get(r"https://www.nseindia.com/api/live-analysis-variations?index=loosers",headers=headers)
+    st_code = res.status_code
+if res.status_code == 200:
+    for val in sec_mappings.values():
+        df = pd.DataFrame(res.json()[val]['data'])
         if df.shape[0] > 0:
             dff = df[cols]
             dff.columns = ncols
             dff['STATUS'] = 'Looser'
-            looser_dict[key] = dff
+            looser_dict[val] = dff
         else:
-            looser_dict[key] = df
+            looser_dict[val] = df
+    df_dict['Loosers'] = looser_dict
 
-df_dict['Gainers'] = gainer_dict
-df_dict['Loosers'] = looser_dict
 
-option = st.selectbox(
+if len(df_dict) > 0:
+    option = st.selectbox(
         "Select Index",
         ("NIFTY 50", "BANK NIFTY", "NIFTY NEXT 50", "Securities > Rs 20", "Securities < Rs 20", "F&O Securities","All Securities")
         )
-
-#print(option)
-#print(type(option))
-# data_dict = read_data()
-l_df = df_dict['Loosers'][sec_mappings[option]]
-g_df = df_dict['Gainers'][sec_mappings[option]]
-
-st.write('Top Gainers Today')
-if g_df.shape[0] > 0:
-    st.write(g_df)
+    l_df = df_dict['Loosers'][sec_mappings[option]]
+    g_df = df_dict['Gainers'][sec_mappings[option]]
+    st.write('Top Gainers Today')
+    if g_df.shape[0] > 0:
+        st.write(g_df)
+    else:
+        st.write('No Gainers')
+    st.write('Top Loosers Today')
+    if l_df.shape[0] > 0:
+        st.write(l_df)
+    else:
+        st.write('No Loosers')
+    dff = pd.concat([g_df, l_df])
+    st.bar_chart(dff, x='SYMBOL', y='PERCHANGE', color='STATUS')
 else:
-    st.write('No Gainers')
-st.write('Top Loosers Today')
-if l_df.shape[0] > 0:
-    st.write(l_df)
-else:
-    st.write('No Loosers')
-
-dff = pd.concat([g_df, l_df])
-st.bar_chart(dff, x='SYMBOL', y='PERCHANGE', color='STATUS')
+    st.write('Error loading data from NSE')
